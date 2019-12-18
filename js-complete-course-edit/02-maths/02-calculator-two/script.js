d62de3c8a8761b2687215df476e004b56a29b853
Thibence <salencethibaut95@gmail.com>
2019-12-18 14:58:18 +0000

    been working in the wrong folder the whole day, it's these kind of mistakes that'll be the death of me one day

--- js-complete-course-edit/02-maths/02-calculator-two/script.js
+++ js-complete-course-edit/02-maths/02-calculator-two/script.js
@@ -1,3 +1,4 @@
+
 /* becode/javascript
  *
  * /02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
@@ -9,10 +10,33 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-function test() {
-    return true;
+function calc(shithead) {
+    // to get the value of an input: document.getElementById("element-id").value
+var number1 = +document.getElementById("op-one").value;
+var number2 = +document.getElementById("op-two").value;
+
+    switch (shithead) {
+        case 'addition':
+            var addition = number1 + number2;
+            alert(addition);
+            break;
+
+        case 'subtraction':
+            var sub = number1 - number2;
+            alert(sub);
+            break;
+
+        case 'multiplication':
+            var mult = number1 * number2;
+            alert(mult);
+            break;
+
+        case 'division':
+            var divide = number1 / number2;
+            alert(divide);
+            break;
+
+    }
+
 }
 
-(function() {
-    return true;
-})();
