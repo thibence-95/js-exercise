8eed63548bcde96f3272de15f5a7240a2c761dcc
Thibaut Salence <salencethibaut95@gmail.com>
2019-12-19 16:59:25 +0100

    completed 4.1 easily

--- js-complete-course-edit/04-dates/01-title-by-hour-one/script.js
+++ js-complete-course-edit/04-dates/01-title-by-hour-one/script.js
@@ -11,8 +11,14 @@
 
 (function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    var time = new Date().getHours();
 
-    // your code here
+    if (time <= 18) {
+        document.getElementById("target").innerHTML = ("Hello!");
+    }
+    
+    else {
+        document.getElementById("target").innerHTML = ("Good evening :)");
+    }
 
 })();
