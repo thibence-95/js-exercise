4183be6b7f714cb3ffeb6815f9821ddb42ddbf3e
Thibence <salencethibaut95@gmail.com>
2019-12-30 11:26:29 +0000

    having hard times, getting help from the myth the man the legend, Eddy G

--- js-complete-course-edit/05-arrays/09-rand-array-stats/script.js
+++ js-complete-course-edit/05-arrays/09-rand-array-stats/script.js
@@ -12,12 +12,37 @@
 (function() {
 
     document.getElementById("run").addEventListener("click", function() {
-        var numbers = [];
-        for (var i = 0; i <=100; i++) {
-            numbers.push(i);
-        }
 
-        
+        //establishing random nrs between 1 - 100
+
+        //Math.floor rounds up number/
+        let rdm1 = Math.floor(Math.random()*100);
+        let rdm2 = Math.floor(Math.random()*100);
+        let rdm3 = Math.floor(Math.random()*100);
+        let rdm4 = Math.floor(Math.random()*100);
+        let rdm5 = Math.floor(Math.random()*100);
+        let rdm6 = Math.floor(Math.random()*100);
+        let rdm7 = Math.floor(Math.random()*100);
+        let rdm8 = Math.floor(Math.random()*100);
+        let rdm9 = Math.floor(Math.random()*100);
+        let rdm10 = Math.floor(Math.random()*100);
+
+        //putting rdm nrs in array
+        let rdm = [rdm1, rdm2, rdm3, rdm4, rdm5, rdm6, rdm7, rdm8, rdm9, rdm10];
+
+        //inserting into HTML table
+        document.getElementById("n-1").innerHTML = rdm1;
+        document.getElementById("n-1").innerHTML = rdm2;
+        document.getElementById("n-1").innerHTML = rdm3;
+        document.getElementById("n-1").innerHTML = rdm4;
+        document.getElementById("n-1").innerHTML = rdm5;
+        document.getElementById("n-1").innerHTML = rdm6;
+        document.getElementById("n-1").innerHTML = rdm7;
+        document.getElementById("n-1").innerHTML = rdm8;
+        document.getElementById("n-1").innerHTML = rdm9;
+        document.getElementById("n-1").innerHTML = rdm10;
+
+        let sort = rdm.sort((a, b) => {return a-b;});
     });
 
 })();
