bffb233ec5cf4c4d03a355a740c3f2b18a43fc31
Thibence <salencethibaut95@gmail.com>
2020-01-02 08:58:07 +0000

    haha who was I kidding by signing up for this lol

--- js-complete-course-edit/06-dom/04-attr-create/script.js
+++ js-complete-course-edit/06-dom/04-attr-create/script.js
@@ -7,10 +7,20 @@
  */
 
 // NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+// You will have time to focus on it later. 8==D~
 
 (function() {
 
-    // your code here
+    let src = document.getElementById("source"); //defines the element that has targeted attr
+    let valium = src.attributes.getNamedItem("data-image").value; //gets value of said attr
+    let crtImg = document.createElement("img"); //creates an image element
+    let addImg = document.getElementById("target").appendChild(crtImg); //adds our image element to .target element
+    let crtAtb = document.createAttribute("src"); //creates new attr within img
+
+    crtAtb.value = crtImg;
+    addImg.setAttributeNode(crtAtb);
+    src.parentNode.removeChild("source");
+
+
 
 })();
