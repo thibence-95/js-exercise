bd4a9fc1bc371ae6f5f31751a0d70b51dc751121
Thibence <salencethibaut95@gmail.com>
2020-01-08 16:49:53 +0100

    thank god for Ann

--- js-complete-course-edit/09-fetch/02-list-to-template/script.js
+++ js-complete-course-edit/09-fetch/02-list-to-template/script.js
@@ -10,5 +10,26 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
+        fetch("http://localhost:12345/_shared/api.json")
+
+            .then((response) => {
+                return response.json();
+            })
+            .then((data)=> {
+                data.heroes.forEach(thisFunction);
+
+                function thisFunction(item, index) {
+                    //console.log(item, index);
+                    let itm = document.getElementById("tpl-hero");
+                    let clone = itm.content.cloneNode(true); //true takes complete template (deep)
+
+                    clone.querySelector(".name").innerHTML = item.name;
+                    clone.querySelector(".alter-ego").innerHTML = item.alterEgo;
+                    clone.querySelector(".powers").innerHTML = item.abilities;
+
+                    document.getElementById("target").appendChild(clone);
+                }
+            })
+    })
 })();
