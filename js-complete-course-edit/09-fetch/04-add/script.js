0dd860fc5a9001146f6a563adaef29c8704e69c7
Thibence <salencethibaut95@gmail.com>
2020-01-09 16:21:10 +0100

    done 9.4 thanks to steve

--- js-complete-course-edit/09-fetch/04-add/script.js
+++ js-complete-course-edit/09-fetch/04-add/script.js
@@ -10,5 +10,39 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let newHero = {};
+
+    fetch("http://localhost:12345/_shared/api.json")
+
+    .then((response) => {
+        return response.json();
+    })
+
+    .then(function (data) {
+        document.getElementById("run").addEventListener("click", function () {
+
+            let heroName = document.getElementById("hero-name").value;
+            let altEgo = document.getElementById("hero-alter-ego").value;
+            let heroPower = document.getElementById("hero-powers").value;
+
+            newHero ["id"] = data.heroes.length +1;
+            newHero ["name"] = heroName;
+            newHero ["alterEgo"] = altEgo;
+            newHero ["abilities"] = []; //
+
+            newHero.abilities.push(heroPower);
+
+            data.heroes.push(newHero);
+            console.log(data.heroes)
+    });
+
+
+
+
+
+        //console.log(heroName, altEgo, heroPower);
+    })
+
+
+
 })();
